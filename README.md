164c9db77e54188f41d0300ce38edd53b0be0e89
Robert Benjamin <rotberj@gmail.com>
2016-05-03 00:50:09 -0700

    Build first component, update README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hue-hue-vue
 Vue.js is too cool not to play with. Let's do this!
+
+## App Idea
+"Robert's favorite albums." Allow searching for music albums and saving of albums with correct metadata.
+
+- Google or Spotify API for search.
+- Firebase for backend.
--- components/component-a.vue
+++ components/component-a.vue
@@ -1,16 +1,27 @@
 <template>
-  <h1>Hello World!</h1>
-  <p>Here's some shitty writing I wrote.</p>
-  <input v-model="message">
-  <p>{{ message }}</p>
+  <div class="component-a">
+    <h1>Hello World!</h1>
+    <p>Here's some shitty writing I wrote.</p>
+    <input v-model="message">
+    <p>{{ message }}</p>
+  </div>
 </template>
 
 <!-- JavScript here -->
 <script>
-
+module.exports = {
+   data: function() {
+    return {
+      message: "Oh shit waddup."
+    }
+   }
+}
 </script>
 
 <!-- Add `lang='sass'` to the style tag for preprocessing -->
 <style>
-  
+.component-a {
+  width: 600px;
+  margin: 0 auto;
+}
 </style>
