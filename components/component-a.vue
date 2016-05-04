fbbdc7227cfa5f320c5c21add9285b2e0c1ded0a
Robert Benjamin <rotberj@gmail.com>
2016-05-04 02:38:18 -0700

    Tutorial todo app

--- components/component-a.vue
+++ components/component-a.vue
@@ -1,20 +1,39 @@
 <template>
   <div class="component-a">
-    <h1>Hello World!</h1>
-    <p>Here's some shitty writing I wrote.</p>
-    <input v-model="message">
-    <p>{{ message }}</p>
+    <h1>Todo List</h1>
+    <input v-model="newTodo" v-on:keyup.enter="addTodo">
+    <ul>
+      <li v-for="todo in todos">
+        <span>{{ todo.text }}</span>
+        <button v-on:click="removeTodo($index)"></button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <!-- JavScript here -->
 <script>
 module.exports = {
-   data: function() {
+  data: function() {
     return {
-      message: "Oh shit waddup."
+      newTodo: "",
+      todos: [
+        { text: "Add some todos" }
+      ]
     }
-   }
+  },
+  methods: {
+    addTodo: function() {
+      var text = this.newTodo.trim()
+      if (text) {
+        this.todos.push({ text: text })
+        this.newTodo = ""
+      }
+    },
+    removeTodo: function(index) {
+      this.todos.splice(index, 1)
+    }
+  }
 }
 </script>
 
